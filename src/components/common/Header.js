ecefa74025a4c9990a9d97ebad9b325121d7ce8f
Emily Morgado <emilyannemorgado@gmail.com>
2018-03-10 13:30:45 -0800

    Create Header component with hard coded message

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -3,9 +3,18 @@ import React from 'react';
 const Header = () => {
   return (
     <div>
-      <h1>HEADER</h1>
+      <h1 style={styles.headerStyle}>
+        Welcome to BookNook!
+      </h1>
     </div>
   );
+}
+
+const styles = {
+  headerStyle: {
+    padding: '20px',
+    fontSize: 50
+  }
 }
 
 export { Header };
