c9d480a66dae7fcbcbc1a3e20636b1ec3ac14cdf
Emily Morgado <emilyannemorgado@gmail.com>
2018-03-10 12:58:31 -0800

    Create button component (with hard coded text)

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -3,9 +3,22 @@ import React from 'react';
 const Button = () => {
   return (
     <div>
-      <h1>BUTTON</h1>
+      <button style={styles.buttonStyle}>
+        SIGN IN
+      </button>
     </div>
   );
+}
+
+const styles = {
+  buttonStyle: {
+    backgroundColor: '#73e03c',
+    height: '40px',
+    width: '300px',
+    borderRadius: 20,
+    color: '#fff',
+    fontSize: 16
+  }
 }
 
 export { Button };
