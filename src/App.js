e25fec0a164353e7163a9b730f9506a814195adc
Emily Morgado <emilyannemorgado@gmail.com>
2018-03-12 16:56:09 -0700

    Clean up spacing and imports

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import Login from './components/Login';
+import Login from './components/Login/Login';
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-      <Login />
+        <Login />
       </div>
     );
   }
