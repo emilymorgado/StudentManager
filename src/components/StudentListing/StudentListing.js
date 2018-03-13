23f33a839a40d227a304581d83d34a202dd4e99b
Emily Morgado <emilyannemorgado@gmail.com>
2018-03-12 20:21:33 -0700

    Student Listing page displays data correctly, styles have been fixed, small header created

--- src/components/StudentListing/StudentListing.js
+++ src/components/StudentListing/StudentListing.js
@@ -1,32 +1,82 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Nav } from '../common';
+import { Nav, NameHeader, StudentList } from '../common';
 
 class StudentListing extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = { students: [] };
     this.loadStudents = this.loadStudents.bind(this);
+    this.renderStudents = this.renderStudents.bind(this);
   }
 
   loadStudents() {
-    const payload = {
-      url: 'https://api-qa.booknooklearning.com/schools/32/students',
-      auth: this.props.token
-    };
+    if (this.state.students.length == 0) {
+      const payload = {
+        url: 'https://api-qa.booknooklearning.com/schools/32/students',
+        auth: this.props.token
+      };
 
-    axios.get(payload.url, {
-      'headers': {'Authorization': payload.auth}})
-    .then((res) => {console.log(res)})
-    .catch((err) => {console.log(err)})
+      axios.get(payload.url, {
+        'headers': {'Authorization': payload.auth}})
+      .then(res => {
+        this.setState({ students: res.data })
+      })
+      .catch(err => {console.log(err)})
+    }
   }
+
+  renderStudents() {
+    return this.state.students.map(student =>
+      (
+        <StudentList
+          key={student.id}
+          firstName={student.first_name}
+          lastName={student.last_name}
+          grade={student.grade_level.sequence}
+          readingLevel={student.reading_level.sequence}
+        />
+    ))}
 
   render() {
     return (
       <div onLoad={this.loadStudents()}>
         <Nav />
+        <NameHeader name={'Emily Morgado'} />
+        <div style={styles.containerStyle}>
+          <p style={styles.studentStyle}>STUDENTS</p>
+          <p style={styles.gradeStyle}>GRADE</p>
+          <p style={styles.readingStyle}>READING LEVEL</p>
+        </div>
+        <div>
+          {this.renderStudents()}
+        </div>
       </div>
     );
+  }
+}
+
+const styles = {
+  containerStyle: {
+    height: '50px',
+    width: '1200px',
+    block: 'inline',
+    fontSize: 16,
+    outline: 0,
+    display: 'flex',
+    color: '#444',
+    marginLeft: '80px'
+  },
+  studentStyle: {
+    marginLeft: '10px',
+    textAlign: 'left',
+    width: '400px'
+  },
+  gradeStyle: {
+    width: '400px'
+  },
+  readingStyle: {
+    width: '400px'
   }
 }
 
--- src/components/common/StudentList.js
+++ src/components/common/StudentList.js
@@ -1,23 +1,39 @@
 import React from 'react';
 
-const StudentList = ({ student }) => {
-  const { firstName, lastName, grade, readingLevel } = student;
+const StudentList = ({ firstName, lastName, grade, readingLevel }) => {
+  const { containerStyle, nameStyle, gradeStyle, readingStyle } = styles;
 
   return (
-    <div style={styles.containerStyle}>
-      <p>{firstName}</p>
-      <p>{lastName}</p>
-      <p>{grade}</p>
-      <p>{readingLevel}</p>
+    <div style={containerStyle}>
+      <p style={nameStyle}>{firstName} {lastName}</p>
+      <p style={gradeStyle}>{grade}</p>
+      <p style={readingStyle}>{readingLevel}</p>
     </div>
   );
 }
 
 const styles = {
   containerStyle: {
-    height: '40px',
+    height: '50px',
+    width: '1200px',
+    block: 'inline',
     fontSize: 16,
-    outline: 0
+    outline: 0,
+    display: 'flex',
+    color: '#444',
+    border: '1px solid #dbdbdb',
+    marginLeft: '80px'
+  },
+  nameStyle: {
+    marginLeft: '10px',
+    textAlign: 'left',
+    width: '400px'
+  },
+  gradeStyle: {
+    width: '400px'
+  },
+  readingStyle: {
+    width: '400px'
   }
 }
 
